5c22d4eaf59fd84b246381822aa0d0db6f05630e
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-05-17 11:09:40 +0200

    modifiche

--- simulazioneterzomodulo/src/App.js
+++ simulazioneterzomodulo/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Home" element={<Home />} />
         <Route path="/posts/:id" element={<Post />} />
       </Routes>
     </>
--- simulazioneterzomodulo/src/Home.js
+++ simulazioneterzomodulo/src/Home.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import NavBar from './components/navbar'
 
 const Home = () => {
   return (
+    <main>
+    <NavBar />
     <div>Home</div>
+    </main>
   )
 }
 
