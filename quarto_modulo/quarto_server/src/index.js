261e117a7489b9242b40a16b52c3c9ef37a68b46
SonKristian <103516057+SonKristian@users.noreply.github.com>
2023-06-01 14:47:24 +0200

    Primo commit 01-06

--- quarto_modulo/quarto_server/src/index.js
+++ quarto_modulo/quarto_server/src/index.js
@@ -1,6 +1,13 @@
 import express from 'express'
+import session from 'express-session'
 const app = express()
 const port = 3000
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 
 import bodyParser from 'body-parser'
 app.use(bodyParser.json())
@@ -16,6 +23,8 @@ app.get('/users/:id', user.get)
 app.put('/users/:id', user.update)
 app.delete('/users/:id', user.remove)
 app.post('/users', user.create)
+app.post('/register', user.register)
+// app.get('/login', user.login)
 
 app.get('/todos', todo.getAll)
 app.get('/todos/search', todo.search)
